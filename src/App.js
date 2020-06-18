012b250d40f4dd7159db768797652e53231bbb28
romkravets <romann.kravets@gmail.com>
2020-06-18 13:01:20 +0300

    start

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header><a href="/">React Shoping Cart</a></header>
+      <main>
+        Product List
+      </main>
+      <footer>
+        footer
+      </footer>
+    </div>
+  );
+}
+
+export default App;
