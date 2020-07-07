12aac1755fd5947ad59c338d95a2c98f7992d753
romkravets <romann.kravets@gmail.com>
2020-07-07 16:44:00 +0300

    form

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,7 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import formattCurrency from '../util';
 
 class Cart extends Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         name: '',
+         email: '',
+         address: '',
+         showCheckout: false,
+      }
+   }
+
+   handleInput = (e) => {
+      this.setState({[e.target.name]: e.target.value});
+   };
+
+   createOrder = (e) => {
+      e.preventDefault();
+      const order = {
+         name: this.state.name,
+         email: this.state.email,
+         address: this.state.address,
+         cartItems: this.props.cartItems,
+      };
+      this.props.createOrder(order);
+   };
+
    render() {
       const {cartItems} = this.props;
       return (
@@ -31,6 +56,7 @@ class Cart extends Component {
                </ul>
             </div>
                {cartItems.length !== 0 && (
+                   <Fragment>
                   <div className="cart">
                      <div className="total">
                         <div>
@@ -39,9 +65,49 @@ class Cart extends Component {
                               cartItems.reduce((a, c) => a + (c.price*c.count), 0)
                               )}
                         </div>
-                        <button className="button primary">Proceed</button>
+                        <button className="button primary" onClick={()=> {
+                           this.setState({showCheckout: true})
+                        }}
+                        >Proceed
+                        </button>
                      </div>
                </div>
+               {this.state.showCheckout && (
+                   <div className="cart">
+                     <form onSubmit={this.createOrder}>
+                        <ul className="form-container">
+                           <li>
+                              <label>Email</label>
+                              <input
+                                  name="email"
+                                  type="email"
+                                  required
+                                  onChange={this.handleInput}/>
+                           </li>
+                           <li>
+                              <label>Name</label>
+                              <input
+                                  name="name"
+                                  type="text"
+                                  required
+                                  onChange={this.handleInput}/>
+                           </li>
+                           <li>
+                              <label>Address</label>
+                              <input
+                                  name="address"
+                                  type="text"
+                                  required
+                                  onChange={this.handleInput}/>
+                           </li>
+                           <li>
+                              <button className="button primary" type='submit'>Checkout</button>
+                           </li>
+                        </ul>
+                     </form>
+                  </div>
+                  )}
+                   </Fragment>
                )}
          </div>
       </div>
