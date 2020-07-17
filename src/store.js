cfac21eb91bab0b4149d04ee30deb6f3047c3146
romkravets <romann.kravets@gmail.com>
2020-07-17 20:15:21 +0300

    create order list

--- src/store.js
+++ src/store.js
@@ -2,12 +2,14 @@ import {createStore, applyMiddleware, compose, combineReducers} from "redux";
 import thunk from "redux-thunk";
 import { productReducer } from "./reducers/productReducer";
 import {cartReducer} from "./reducers/cartReducer";
+import {orderReducer} from "./reducers/orderReducer";
 
 const initialState = {};
 const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : (null || compose);
 const store = createStore(combineReducers({
     products: productReducer,
     cart: cartReducer,
+    order: orderReducer,
 }),
     initialState,
     composeEnhancers(applyMiddleware(thunk))
--- src/util.js
+++ src/util.js
@@ -1,3 +1,3 @@
-export default function formattCurrency(num) {
+export default function formatCurrency(num) {
    return "$" + Number(num.toFixed(1)).toLocaleString() + "";
 }
