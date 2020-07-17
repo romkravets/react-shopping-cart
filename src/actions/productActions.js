e590c0433590056b8e6f0722b81fa9b7c3020ce4
romkravets <romann.kravets@gmail.com>
2020-07-17 14:05:34 +0300

    add

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -2,7 +2,12 @@ import {FETCH_PRODUCTS} from "../types";
 
 export const fetchProducts = () => async (dispatch) => {
 
-   const res =  await fetch("/api/products");
+   const res =  await fetch("/api/products", {
+       headers : {
+           'Content-Type': 'application/json',
+           'Accept': 'application/json'
+       }
+   });
    const data = await res.json();
    dispatch({
        type: FETCH_PRODUCTS,
