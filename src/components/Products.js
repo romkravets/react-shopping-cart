a9abbacf8d6a8812a70c489da8dfd7fdc8baee30
romkravets <romann.kravets@gmail.com>
2020-07-17 09:52:41 +0300

    react-reveal

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,31 +1,34 @@
 import React, { Component } from 'react'
 import formatCurrency from '../util';
+import Fade from 'react-reveal/Fade';
 
 export default class Products extends Component {
    render() {
       return (
          <div>
-            <ul className="products">
-               {this.props.products.map((product) => (
-                  <li key={product._id}>
-                     <div className="product"
-                     >
-                        <a href={"#" + product._id}>
-                           <img src={product.image} alt={product.title}/>
-                           <p>
-                              {product.title}
-                           </p>
-                           </a>
-                           <div className="product-price">
-                              <div>{ formatCurrency(product.price)}</div>
-                              <button className="button primary" onClick={() => this.props.addToCart(product)}>
-                                 Add To Cart
-                              </button>
-                           </div>
-                     </div>
-                  </li>
-               ))}
-            </ul>
+             <Fade bottom cascade={true}>
+                <ul className="products">
+                   {this.props.products.map((product) => (
+                      <li key={product._id}>
+                         <div className="product"
+                         >
+                            <a href={"#" + product._id}>
+                               <img src={product.image} alt={product.title}/>
+                               <p>
+                                  {product.title}
+                               </p>
+                               </a>
+                               <div className="product-price">
+                                  <div>{ formatCurrency(product.price)}</div>
+                                  <button className="button primary" onClick={() => this.props.addToCart(product)}>
+                                     Add To Cart
+                                  </button>
+                               </div>
+                         </div>
+                      </li>
+                   ))}
+                </ul>
+             </Fade>
          </div>
       )
    }
